9dbb5a62833120175e5a13cf7220b1e1f9d1ab57
Jefferson-Bueno-Da-Silva <jeffersonbuenoprofissional@gmail.com>
2021-04-14 11:29:54 -0300

    Tela de login, com usuario e senha, botão de fechar e movimentação na tela

--- desktop/Pimbeck/Login.cs
+++ desktop/Pimbeck/Login.cs
@@ -12,9 +12,76 @@ namespace Pimbeck
 {
     public partial class Login : Form
     {
+        bool mouseDown;
+        private Point offset;
         public Login()
         {
             InitializeComponent();
+        }
+
+        private void textBox1_Leave(object sender, EventArgs e)
+        {
+            if(textBox1.Text == "")
+            {
+                textBox1.Text = "Email";
+            }
+            textBox1.ForeColor = Color.DarkGray;
+        }
+
+        private void textBox1_Enter(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "Email")
+            {
+                textBox1.Text = null;
+            }
+            textBox1.ForeColor = Color.Black;
+        }
+
+        private void textBox2_Leave(object sender, EventArgs e)
+        {
+            if (textBox2.Text == "")
+            {
+                textBox2.Text = "Senha";
+                this.textBox2.UseSystemPasswordChar = false;
+            }
+            textBox2.ForeColor = Color.DarkGray;
+        }
+
+        private void textBox2_Enter(object sender, EventArgs e)
+        {
+            if (textBox2.Text == "Senha")
+            {
+                textBox2.Text = null;
+                this.textBox2.UseSystemPasswordChar = true;
+            }
+            textBox2.ForeColor = Color.Black;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Application.Exit();
+        }
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            offset.X = e.X;
+            offset.Y = e.Y;
+            mouseDown = true;
+        }
+
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if(mouseDown == true)
+            {
+                Point currentScreenPos = PointToScreen(e.Location);
+                Location = new Point(currentScreenPos.X - offset.X, currentScreenPos.Y - offset.Y);
+
+            }
+        }
+
+        private void panel1_MouseUp(object sender, MouseEventArgs e)
+        {
+            mouseDown = false;
         }
     }
 }
