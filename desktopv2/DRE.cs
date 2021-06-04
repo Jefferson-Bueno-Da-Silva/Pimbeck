1d694c7300a945ddfef5d093da6fabf2c432c3eb
Illya Protopopov <ilyadesh28@gmail.com>
2021-06-04 20:42:17 -0300

    Tela DRE

---
+++ desktopv2/DRE.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace desktopv2
+{
+    public partial class DRE : Form
+    {
+        public DRE()
+        {
+            InitializeComponent();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void DRE_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
