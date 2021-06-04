3a628d3ea2238047026d47d3b3ed4487518d7aa4
Jandrade2k <dev.jandrade@gmail.com>
2021-06-04 14:04:17 -0300

    Tela Financeiro V2

--- desktopv2/Financeiro.Designer.cs
+++ desktopv2/Financeiro.Designer.cs
@@ -29,12 +29,156 @@ namespace desktopv2
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Financeiro));
+            this.panel17 = new System.Windows.Forms.Panel();
+            this.moveToBack = new System.Windows.Forms.PictureBox();
+            this.H1 = new System.Windows.Forms.Label();
+            this.ContasReceber = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.moveToBack)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel17
+            // 
+            this.panel17.Location = new System.Drawing.Point(0, 0);
+            this.panel17.Margin = new System.Windows.Forms.Padding(4);
+            this.panel17.Name = "panel17";
+            this.panel17.Size = new System.Drawing.Size(857, 29);
+            this.panel17.TabIndex = 2;
+            // 
+            // moveToBack
+            // 
+            this.moveToBack.BackColor = System.Drawing.Color.Transparent;
+            this.moveToBack.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("moveToBack.BackgroundImage")));
+            this.moveToBack.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.moveToBack.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.moveToBack.Location = new System.Drawing.Point(24, 29);
+            this.moveToBack.Margin = new System.Windows.Forms.Padding(4);
+            this.moveToBack.Name = "moveToBack";
+            this.moveToBack.Size = new System.Drawing.Size(26, 32);
+            this.moveToBack.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.moveToBack.TabIndex = 7;
+            this.moveToBack.TabStop = false;
+            // 
+            // H1
+            // 
+            this.H1.AutoSize = true;
+            this.H1.BackColor = System.Drawing.Color.Transparent;
+            this.H1.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.H1.ForeColor = System.Drawing.SystemColors.GrayText;
+            this.H1.Location = new System.Drawing.Point(58, 29);
+            this.H1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.H1.Name = "H1";
+            this.H1.Size = new System.Drawing.Size(133, 32);
+            this.H1.TabIndex = 6;
+            this.H1.Text = "Financeiro";
+            // 
+            // ContasReceber
+            // 
+            this.ContasReceber.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.ContasReceber.Image = ((System.Drawing.Image)(resources.GetObject("ContasReceber.Image")));
+            this.ContasReceber.Location = new System.Drawing.Point(24, 151);
+            this.ContasReceber.Name = "ContasReceber";
+            this.ContasReceber.Size = new System.Drawing.Size(250, 150);
+            this.ContasReceber.TabIndex = 8;
+            this.ContasReceber.Text = "Contas a receber";
+            this.ContasReceber.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.Location = new System.Drawing.Point(304, 151);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(250, 150);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Contas a pagar";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button2.Image = ((System.Drawing.Image)(resources.GetObject("button2.Image")));
+            this.button2.Location = new System.Drawing.Point(580, 151);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(250, 150);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Boletos";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button3.Image = ((System.Drawing.Image)(resources.GetObject("button3.Image")));
+            this.button3.Location = new System.Drawing.Point(580, 319);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(250, 150);
+            this.button3.TabIndex = 13;
+            this.button3.Text = "Integração Bancaria";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button4.Image = ((System.Drawing.Image)(resources.GetObject("button4.Image")));
+            this.button4.Location = new System.Drawing.Point(304, 319);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(250, 150);
+            this.button4.TabIndex = 12;
+            this.button4.Text = "Fluxo de caixa";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button5.Image = ((System.Drawing.Image)(resources.GetObject("button5.Image")));
+            this.button5.Location = new System.Drawing.Point(24, 319);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(250, 150);
+            this.button5.TabIndex = 11;
+            this.button5.Text = "Demonstrativo de resultados";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // Financeiro
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Financeiro";
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(29)))), ((int)(((byte)(29)))));
+            this.ClientSize = new System.Drawing.Size(864, 591);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.ContasReceber);
+            this.Controls.Add(this.moveToBack);
+            this.Controls.Add(this.H1);
+            this.Controls.Add(this.panel17);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Financeiro";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Funcionario - adicionar func.";
+            ((System.ComponentModel.ISupportInitialize)(this.moveToBack)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private System.Windows.Forms.Panel panel17;
+        private System.Windows.Forms.PictureBox moveToBack;
+        private System.Windows.Forms.Label H1;
+        private System.Windows.Forms.Button ContasReceber;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
     }
 }
