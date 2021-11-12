5fd8885653c0b4a2ab4908cb4f1a768258f8fd7e
Jefferson-Bueno-Da-Silva <jeffersonbuenoprofissional@gmail.com>
2021-11-12 09:25:49 -0300

    remove usuarios

--- desktopv2/recepcao.Designer.cs
+++ desktopv2/recepcao.Designer.cs
@@ -49,27 +49,25 @@ namespace desktopv2
             this.panel14 = new System.Windows.Forms.Panel();
             this.label14 = new System.Windows.Forms.Label();
             this.panel15 = new System.Windows.Forms.Panel();
-            this.panel16 = new System.Windows.Forms.Panel();
-            this.label15 = new System.Windows.Forms.Label();
-            this.button2 = new System.Windows.Forms.Button();
-            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
-            this.panel2 = new System.Windows.Forms.Panel();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.panel16 = new System.Windows.Forms.Panel();
+            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.label15 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
             this.MenuPanel.SuspendLayout();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.panel17.SuspendLayout();
             this.panel14.SuspendLayout();
             this.panel15.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.panel16.SuspendLayout();
             this.panel2.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // MenuPanel
             // 
             this.MenuPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(18)))), ((int)(((byte)(18)))), ((int)(((byte)(18)))));
-            this.MenuPanel.Controls.Add(this.button2);
             this.MenuPanel.Controls.Add(this.button1);
             this.MenuPanel.Controls.Add(this.AlmoxarifadoBtn);
             this.MenuPanel.Controls.Add(this.ReservasBtn);
@@ -93,7 +91,7 @@ namespace desktopv2
             this.button1.ForeColor = System.Drawing.SystemColors.ButtonFace;
             this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
             this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.button1.Location = new System.Drawing.Point(3, 441);
+            this.button1.Location = new System.Drawing.Point(0, 401);
             this.button1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(230, 40);
@@ -110,7 +108,7 @@ namespace desktopv2
             this.AlmoxarifadoBtn.ForeColor = System.Drawing.SystemColors.ButtonFace;
             this.AlmoxarifadoBtn.Image = ((System.Drawing.Image)(resources.GetObject("AlmoxarifadoBtn.Image")));
             this.AlmoxarifadoBtn.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.AlmoxarifadoBtn.Location = new System.Drawing.Point(3, 395);
+            this.AlmoxarifadoBtn.Location = new System.Drawing.Point(0, 355);
             this.AlmoxarifadoBtn.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.AlmoxarifadoBtn.Name = "AlmoxarifadoBtn";
             this.AlmoxarifadoBtn.Size = new System.Drawing.Size(230, 40);
@@ -127,7 +125,7 @@ namespace desktopv2
             this.ReservasBtn.ForeColor = System.Drawing.SystemColors.ButtonFace;
             this.ReservasBtn.Image = ((System.Drawing.Image)(resources.GetObject("ReservasBtn.Image")));
             this.ReservasBtn.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.ReservasBtn.Location = new System.Drawing.Point(3, 349);
+            this.ReservasBtn.Location = new System.Drawing.Point(0, 310);
             this.ReservasBtn.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.ReservasBtn.Name = "ReservasBtn";
             this.ReservasBtn.Size = new System.Drawing.Size(230, 40);
@@ -144,7 +142,7 @@ namespace desktopv2
             this.ClientesBtn.ForeColor = System.Drawing.SystemColors.ButtonFace;
             this.ClientesBtn.Image = ((System.Drawing.Image)(resources.GetObject("ClientesBtn.Image")));
             this.ClientesBtn.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.ClientesBtn.Location = new System.Drawing.Point(3, 301);
+            this.ClientesBtn.Location = new System.Drawing.Point(0, 264);
             this.ClientesBtn.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.ClientesBtn.Name = "ClientesBtn";
             this.ClientesBtn.Size = new System.Drawing.Size(230, 40);
@@ -335,62 +333,6 @@ namespace desktopv2
             this.panel15.Size = new System.Drawing.Size(529, 491);
             this.panel15.TabIndex = 20;
             // 
-            // panel16
-            // 
-            this.panel16.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(39)))), ((int)(((byte)(39)))));
-            this.panel16.Controls.Add(this.monthCalendar1);
-            this.panel16.Location = new System.Drawing.Point(838, 99);
-            this.panel16.Margin = new System.Windows.Forms.Padding(2);
-            this.panel16.Name = "panel16";
-            this.panel16.Size = new System.Drawing.Size(229, 202);
-            this.panel16.TabIndex = 21;
-            // 
-            // label15
-            // 
-            this.label15.AutoSize = true;
-            this.label15.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label15.ForeColor = System.Drawing.SystemColors.AppWorkspace;
-            this.label15.Location = new System.Drawing.Point(30, 6);
-            this.label15.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label15.Name = "label15";
-            this.label15.Size = new System.Drawing.Size(74, 17);
-            this.label15.TabIndex = 3;
-            this.label15.Text = "Calendario";
-            // 
-            // button2
-            // 
-            this.button2.FlatAppearance.BorderSize = 0;
-            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button2.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.button2.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.button2.Image = ((System.Drawing.Image)(resources.GetObject("button2.Image")));
-            this.button2.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.button2.Location = new System.Drawing.Point(3, 261);
-            this.button2.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(230, 40);
-            this.button2.TabIndex = 13;
-            this.button2.Text = "Usuarios";
-            this.button2.UseVisualStyleBackColor = true;
-            // 
-            // monthCalendar1
-            // 
-            this.monthCalendar1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(39)))), ((int)(((byte)(39)))));
-            this.monthCalendar1.Location = new System.Drawing.Point(2, 40);
-            this.monthCalendar1.Name = "monthCalendar1";
-            this.monthCalendar1.TabIndex = 4;
-            this.monthCalendar1.TitleBackColor = System.Drawing.SystemColors.ActiveBorder;
-            // 
-            // panel2
-            // 
-            this.panel2.BackColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.panel2.Controls.Add(this.label15);
-            this.panel2.Location = new System.Drawing.Point(838, 98);
-            this.panel2.Margin = new System.Windows.Forms.Padding(2);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(229, 30);
-            this.panel2.TabIndex = 20;
-            // 
             // dataGridView1
             // 
             this.dataGridView1.AllowUserToOrderColumns = true;
@@ -404,6 +346,46 @@ namespace desktopv2
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.Size = new System.Drawing.Size(529, 478);
             this.dataGridView1.TabIndex = 41;
+            // 
+            // panel16
+            // 
+            this.panel16.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(39)))), ((int)(((byte)(39)))));
+            this.panel16.Controls.Add(this.monthCalendar1);
+            this.panel16.Location = new System.Drawing.Point(838, 99);
+            this.panel16.Margin = new System.Windows.Forms.Padding(2);
+            this.panel16.Name = "panel16";
+            this.panel16.Size = new System.Drawing.Size(229, 202);
+            this.panel16.TabIndex = 21;
+            // 
+            // monthCalendar1
+            // 
+            this.monthCalendar1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(39)))), ((int)(((byte)(39)))));
+            this.monthCalendar1.Location = new System.Drawing.Point(2, 40);
+            this.monthCalendar1.Name = "monthCalendar1";
+            this.monthCalendar1.TabIndex = 4;
+            this.monthCalendar1.TitleBackColor = System.Drawing.SystemColors.ActiveBorder;
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label15.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.label15.Location = new System.Drawing.Point(30, 6);
+            this.label15.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(74, 17);
+            this.label15.TabIndex = 3;
+            this.label15.Text = "Calendario";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.panel2.Controls.Add(this.label15);
+            this.panel2.Location = new System.Drawing.Point(838, 98);
+            this.panel2.Margin = new System.Windows.Forms.Padding(2);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(229, 30);
+            this.panel2.TabIndex = 20;
             // 
             // recepcao
             // 
@@ -433,10 +415,10 @@ namespace desktopv2
             this.panel14.ResumeLayout(false);
             this.panel14.PerformLayout();
             this.panel15.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.panel16.ResumeLayout(false);
             this.panel2.ResumeLayout(false);
             this.panel2.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -465,7 +447,6 @@ namespace desktopv2
         private System.Windows.Forms.Panel panel17;
         private System.Windows.Forms.Button minimize;
         private System.Windows.Forms.Button close;
-        private System.Windows.Forms.Button button2;
         private System.Windows.Forms.MonthCalendar monthCalendar1;
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.DataGridView dataGridView1;
